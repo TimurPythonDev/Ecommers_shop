d8370f11a85ffd7b7433f5f877dcc94baae6e755
TimurPythonDev <karabayevtimurpython@gmail.com>
2022-04-24 10:09:51 +0300

    images

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,33 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField,ModelForm,ValidationError
 
 from django.contrib import admin
 from .models import *
 
+from PIL import Image
 
 
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400,400)
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['image'].help_text = 'Rasimni yuklayotkanda Hajmiga etbor bering minimum  {} x {}'.format(*self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Yuklangan rasim korsatilgan kichik')
+        # print(img.width,img.height)
+        return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
