76340d87d7dfc429b5e485fdadd248f70b44c068
TimurPythonDev <karabayevtimurpython@gmail.com>
2022-05-15 00:42:00 +0300

    filter

---
+++ app/templatetags/specifications.py
@@ -0,0 +1,58 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+TABLE_HEAD = """
+            <table class="table">
+                <tbody>
+            """
+
+TABLE_TAIL = """
+                </tbody>
+            </table>
+            """
+
+TABLE_CONTENT = """
+                <tr>
+                    <td>{name}</td>
+                    <td>{value}</td>
+                </tr>
+                """
+
+PRODUCT_SPEC = {
+    'notebook':{
+        'Display':'dioganal',
+        'Display type':'display_type',
+        'Protsessor':'processor_freq',
+        'Ram':'ram',
+        'Video CARD':'video',
+        'Akumlyator quvvati':'time_with',
+    },
+    'smartphone':{
+        'Display':'dioganal',
+        'Display type':'display_type',
+        'Ekran olchami':'resolution',
+        'Ram':'ram',
+        'Battareyka sigimi':'accum_volume',
+        'Tashqi hotira':'sd',
+        'Asosiy hotira':'sd_volume_max',
+        'Asosiy kamera MP':'main_cam_mp',
+        'Oldi kamera MP':'frontal_cam_mp',
+    }
+}
+
+def get_product_spec(product,model_name):
+    table_contant = ''
+    for name,value in PRODUCT_SPEC[model_name].items():
+        table_contant += TABLE_CONTENT.format(name=name,value=getattr(product,value))
+    return table_contant
+
+
+@register.filter
+def product_spec(product):
+    model_name = product.__class__._meta.model_name
+
+    return mark_safe(TABLE_HEAD + get_product_spec(product,model_name) + TABLE_TAIL)
